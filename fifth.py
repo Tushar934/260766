b1bbcb0caddd89b19e1a9dc7a90091283bcc74e8
Tushar934 <62945123+Tushar934@users.noreply.github.com>
2021-04-18 16:01:33 +0530

    commit on tuples in python

---
+++ fifth.py
@@ -0,0 +1,14 @@
+#This demo is for Tuple
+
+my_tup=("DD","Dev","Shweta","Lata")
+print(my_tup)
+
+print(my_tup+("Rinkoo","Divyanshu"))  #Concatenation
+print(my_tup)
+
+
+print(my_tup*3)
+print(my_tup[1:3])
+print(my_tup[1:30])
+
+
