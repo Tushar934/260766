961ed3e886e262530f6afc41e73228c577145089
Tushar934 <62945123+Tushar934@users.noreply.github.com>
2021-04-18 15:37:11 +0530

    commit on numbers in python

---
+++ second.py
@@ -0,0 +1,9 @@
+a = 25
+print("type of a", type(a))
+
+b = 20.5
+print("type of b", type(b))
+
+c = 1+3j
+print("type of c", type(c))
+print(" c is a complex number? True of False", isinstance(1+3j,complex))
