93bca5b515df5e577901aa241b8889af02dcc6d2
Tushar934 <62945123+Tushar934@users.noreply.github.com>
2021-04-18 15:52:30 +0530

    commit on lists in python

---
+++ fourth.py
@@ -0,0 +1,14 @@
+#This demo is for extend() method
+aList = [123, 'xyz', 'zara', 'abc', 123];
+bList = [2009, 'manni'];
+a=[10,20,30]
+print(sum(a))
+aList.append(67)
+print(aList)
+aList.pop()
+print(aList)
+aList.remove('xyz')
+print( aList)
+aList.extend(bList)
+print(aList)
+
