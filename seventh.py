4984000158ee8abdd8641fead0ebf6287b014d99
Tushar934 <62945123+Tushar934@users.noreply.github.com>
2021-04-19 21:45:47 +0530

    commit on dictionary in python

---
+++ seventh.py
@@ -0,0 +1,8 @@
+#This demo is for dictionary
+my_dict={1:"Red",2:"Blue",3:"Green"}
+print(my_dict[3])
+print(my_dict.items())
+print(my_dict.keys())
+print(my_dict.values())
+my_dict.update({400:"Magenta"})
+print(my_dict)
