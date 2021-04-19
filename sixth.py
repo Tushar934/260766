8c2ea11fbf0a70528775c0f291ebec57887b81de
Tushar934 <62945123+Tushar934@users.noreply.github.com>
2021-04-19 21:38:56 +0530

    commit on sets in python

---
+++ sixth.py
@@ -0,0 +1,10 @@
+#This demo is for Set
+my_set={1,2,2,5,7,5,10,8,2,1}
+print(my_set)
+#This demo is for Set Methods
+my_set1={1,2,5,7,5,10,8,2,1}
+my_set2={2,5,9,11,15,20}
+print(my_set1)
+print("Union = ",my_set1|my_set2)
+print("Intersection = ",my_set1&my_set2)
+print("Difference = ",my_set1-my_set2)
