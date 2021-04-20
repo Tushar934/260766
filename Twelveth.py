35214e4f9eb63c0a828244ca81c40a3d86991e4c
Tushar934 <62945123+Tushar934@users.noreply.github.com>
2021-04-20 14:29:51 +0530

    commit on while loop in python

---
+++ Twelveth.py
@@ -0,0 +1,17 @@
+# The `while` loop
+is_programmer = True
+while is_programmer:
+    print('Hello, world!')
+    is_programmer = False   #run 1 times
+
+i = 1
+while i<=10:
+    print(f'Repeated {i} times')
+    i += 1
+
+temperature = 15
+
+while temperature<20:
+    print('Heating...')
+    temperature += 1
+#run 5 times
