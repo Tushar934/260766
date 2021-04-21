47a714d878bc27a31b1e61c7d25f1a0ece0f2b76
Tushar934 <62945123+Tushar934@users.noreply.github.com>
2021-04-21 22:34:23 +0530

    commit on user-defined function  in python

---
+++ fifteenth.py
@@ -0,0 +1,19 @@
+def i_return():
+    return 5 + 5
+
+def i_print():
+    print(5 + 5)
+
+result = i_return()
+another = i_print()
+
+print(result)
+print(another)
+
+def fives():
+    addition = 5 + 5
+    print(addition)
+    return addition
+
+result = fives()
+print(result)
