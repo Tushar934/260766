80e80d0c2f5a654e3b356e07ae9ded7dda97907d
Tushar934 <62945123+Tushar934@users.noreply.github.com>
2021-04-24 23:19:01 +0530

    commit on if

---
+++ twentyeight.py
@@ -0,0 +1,9 @@
+num=int(input("Please Enter A Number"))
+if(num>0):
+    print("This is the positive number")
+    print("This is within the if block")
+
+print("This is outside from the if block")
+
+
+
