9574de2675b998850dbc0e1bfa064587722c7600
Tushar934 <62945123+Tushar934@users.noreply.github.com>
2021-04-26 22:28:54 +0530

    commit on file handling on read part

---
+++ thirty.py
@@ -0,0 +1,25 @@
+f=open("e:/evening.txt",'r')
+data=f.read()
+print(data)
+f.close()
+
+f=open("e:/evening.txt",'r')
+lines=f.readlines()
+#print(lines)
+
+for line in lines:
+    print(line,end='')
+f.close()
+
+f=open("e:/evening.txt",'r')
+line1=f.readline()
+print(line1)
+line2=f.readline()
+print(line2)
+line3=f.readline()
+print(line3)
+f.close()
+
+
+
+
